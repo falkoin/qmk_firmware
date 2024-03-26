868edbe00f70829ba6c39bc0675d1ad8a5a83054
Falko Döhring <fdoehring@cocus.com>
2024-03-26 20:37:10 +0100

    reformat keymap

--- keyboards/splitkb/kyria/keymaps/falkoin/keymap.c
+++ keyboards/splitkb/kyria/keymaps/falkoin/keymap.c
@@ -17,12 +17,15 @@ enum custom_keycodes {
 
  const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
- [_LAYER0] = LAYOUT(KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T,                                      KC_Y, KC_U, KC_I, KC_O, KC_P, KC_MINS, 
-                    KC_ESC, KC_A, KC_S, KC_D, KC_F, KC_G,                                      KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, 
-                    KC_LCTL, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_VOLD, KC_MUTE,   KC_NO, KC_VOLU, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, OSL(4), 
+ [_LAYER0] = LAYOUT(KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T,                                      KC_Y, KC_U, KC_I, KC_O, KC_P, KC_MINS,
+                    KC_ESC, KC_A, KC_S, KC_D, KC_F, KC_G,                                      KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT,
+                    KC_LCTL, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_VOLD, KC_MUTE,   KC_NO, KC_VOLU, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, OSL(4),
                                         KC_LALT, TT(1), KC_LGUI, KC_LSFT, MO(3),   KC_RSFT, KC_ENT, KC_SPC, MO(2), KC_NO),
 
-[_LAYER1] = LAYOUT(KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_CIRC, KC_AMPR, KC_ASTR, KC_NO, LALT(KC_S), QK_MACRO_0, KC_GRV, KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_PERC, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, QK_MACRO_1, QK_MACRO_2, KC_LSFT, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, HYPR(KC_DOWN), KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_LALT, KC_LGUI, KC_TRNS, KC_TRNS, KC_TRNS, KC_LCTL, KC_TRNS, KC_NO),
+[_LAYER1] = LAYOUT(KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,                                             KC_CIRC,       KC_AMPR, KC_ASTR, KC_NO,   LALT(KC_S), QK_MACRO_0,
+                   KC_GRV,  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                                           KC_LEFT,       KC_DOWN, KC_UP,   KC_RGHT, QK_MACRO_1, QK_MACRO_2,
+                   KC_LSFT, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,       KC_TRNS, KC_TRNS, HYPR(KC_DOWN), KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,    KC_TRNS,
+                                              KC_TRNS, KC_TRNS, KC_LALT, KC_LGUI, KC_TRNS,       KC_TRNS, KC_TRNS, KC_LCTL,       KC_TRNS, KC_NO),
 
 [_LAYER2] = LAYOUT(KC_0, KC_1, KC_2, KC_3, KC_4, KC_5,                                                           KC_6, KC_7, KC_8, KC_9, KC_TRNS, KC_TRNS,
                    KC_TRNS, KC_TRNS, KC_LCBR, KC_LBRC, KC_LPRN, KC_UNDS,                                         KC_EQL, KC_RPRN, KC_RBRC, KC_RCBR, KC_MINS, KC_TRNS,
